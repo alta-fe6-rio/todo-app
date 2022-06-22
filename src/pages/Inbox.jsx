36b02611eca0a7e4cd4d4d102b0c97660d636af5
Rio Dwi S <riodwisyahputra25@gmail.com>
2022-06-22 23:40:06 +0700

    add some detail

--- src/pages/Inbox.jsx
+++ src/pages/Inbox.jsx
@@ -1,36 +1,87 @@
 /** @format */
 
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Layout from '../components/Layout';
 
 const Inbox = () => {
-	return (
-		<Layout>
-			<div className='m-12 rounded-sm shadow-md'>
-				<div className='bg-slate-300/30 py-3 px-6'>Tasks</div>
-				<div className='grid grid-cols-1 md:grid-cols-3 p-6'>
-					<div>
-						<h1 className='font-semibold'>Item</h1>
-						<div className='pt-8'>
-							<h1>Gym</h1>
+	const [complete, setComplete] = useState('not complete');
+	const [task, setTask] = useState([]);
+	const [remove, setRemove] = useState([]);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	function fetchData() {
+		axios
+			.get('https://api.todoist.com/rest/v1/tasks', { headers: { Authorization: `Bearer ${process.env.REACT_APP_API_KEY}` } })
+			.then((res) => {
+				console.log(res.data);
+				setTask(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			})
+			.finally(() => {
+				setLoading(false);
+			});
+	}
+
+	function handleRemove(id) {
+		axios
+			.delete(`https://api.todoist.com/rest/v1/tasks/${id}`, { headers: { Authorization: `Bearer ${process.env.REACT_APP_API_KEY}` } })
+			.then((res) => {
+				setRemove(res.data);
+				alert('sucessfully deleted');
+			})
+			.catch((err) => {
+				console.log(err);
+			})
+			.finally(() => {
+				setLoading(false);
+			});
+	}
+
+	if (loading) {
+		return <div className='h-screen flex justify-center items-center text-2xl'>Loading...</div>;
+	} else {
+		return (
+			<Layout>
+				{task.map((item, index) => {
+					return (
+						<div className='m-12 rounded-sm shadow-md mb-5' key={index}>
+							<div className='bg-slate-300/30 py-3 px-6'>Tasks</div>
+							<div className='grid grid-cols-1 md:grid-cols-3 p-6'>
+								<div>
+									<h1 className='font-semibold'>Item</h1>
+									<div className='pt-8'>
+										<h1>{item.content}</h1>
+									</div>
+								</div>
+								<div>
+									<h1 className='font-semibold'>Status</h1>
+									<div className='pt-8'>
+										<h1>{complete}</h1>
+									</div>
+								</div>
+								<div>
+									<h1 className='font-semibold'>Action</h1>
+									<div className='pt-8 text-white space-x-2'>
+										<button className='bg-sky-500 py-2 px-4 rounded-sm hover:bg-sky-600'>Complete</button>
+										<button className='bg-red-500 py-2 px-4 rounded-sm hover:bg-red-600' onClick={() => handleRemove(item.id)}>
+											Delete
+										</button>
+									</div>
+								</div>
+							</div>
 						</div>
-					</div>
-					<div>
-						<h1 className='font-semibold'>Status</h1>
-						<div className='pt-8'>
-							<h1>Not Complete</h1>
-						</div>
-					</div>
-					<div>
-						<h1 className='font-semibold'>Action</h1>
-						<div className='pt-8 text-white space-x-2'>
-							<button className='bg-sky-500 py-2 px-4 rounded-sm hover:bg-sky-600'>Complete</button>
-							<button className='bg-red-500 py-2 px-4 rounded-sm hover:bg-red-600'>Delete</button>
-						</div>
-					</div>
-				</div>
-			</div>
-		</Layout>
-	);
+					);
+				})}
+			</Layout>
+		);
+	}
 };
 
 export default Inbox;
--- src/pages/Today.jsx
+++ src/pages/Today.jsx
@@ -1,38 +1,70 @@
 /** @format */
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { BsChatLeft, BsThreeDots } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
 
 import Layout from '../components/Layout';
 
 const Today = () => {
-	return (
-		<Layout>
-			<div className='m-12 rounded-sm'>
-				<div>
-					<h1>Today</h1>
-					<div className='flex justify-end space-x-3 cursor-pointer'>
-						<BsChatLeft />
-						<BsThreeDots />
-					</div>
-				</div>
-				<ul className='mt-4'>
-					<li className='grid grid-cols-2'>
-						<div>
-							<input type='checkbox' />
-							<span className='pl-3'>Gym</span>
+	const [task, setTask] = useState([]);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	function fetchData() {
+		axios
+			.get('https://api.todoist.com/rest/v1/tasks', { headers: { Authorization: `Bearer ${process.env.REACT_APP_API_KEY}` } })
+			.then((res) => {
+				console.log(res.data);
+				setTask(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			})
+			.finally(() => {
+				setLoading(false);
+			});
+	}
+
+	if (loading) {
+		return <div className='h-screen flex justify-center items-center text-2xl'>Loading...</div>;
+	} else {
+		return (
+			<Layout>
+				<div className='m-12 rounded-sm'>
+					<div>
+						<h1>Today</h1>
+						<div className='flex justify-end space-x-3 cursor-pointer'>
+							<BsChatLeft />
+							<BsThreeDots />
 						</div>
-						<span className='flex justify-end text-slate-500'>Personal</span>
-					</li>
-				</ul>
-				<Link to='/create-task'>
-					<div className='mt-8 cursor-pointer hover:bg-slate-50 hover:rounded-sm py-2'>
-						<span className='text-xl'>+</span>
-						<span className='pl-4'>Add a task</span>
 					</div>
-				</Link>
-			</div>
-		</Layout>
-	);
+					<ul className='mt-4'>
+						{task.map((item, index) => {
+							return (
+								<li className='grid grid-cols-2' key={index}>
+									<div>
+										<input type='checkbox' />
+										<span className='pl-3'>{item.content}</span>
+									</div>
+									<span className='flex justify-end text-slate-500'>Personal</span>
+								</li>
+							);
+						})}
+					</ul>
+					<Link to='/create-task'>
+						<div className='mt-8 cursor-pointer hover:bg-slate-50 hover:rounded-sm py-2'>
+							<span className='text-xl'>+</span>
+							<span className='pl-4'>Add a task</span>
+						</div>
+					</Link>
+				</div>
+			</Layout>
+		);
+	}
 };
 
 export default Today;
